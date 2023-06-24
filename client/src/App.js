1b0f3829c89b7a145803bae5cdeff9d43cb1cd41
Siddesh272 <90123105+Siddesh272@users.noreply.github.com>
2023-06-24 21:42:40 +0530

    Book Appointment Done

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,6 @@ import Users from "./pages/admin/Users";
 import Doctors from "./pages/admin/Doctors";
 import Profile from "./pages/doctor/Profile";
 import BookingPage from "./pages/BookingPage";
-import Appointments from "./pages/Appointments";
-import DoctorAppointments from "./pages/doctor/DoctorAppointments";
 function App() {
   const { loading } = useSelector((state) => state.alerts);
   return (
@@ -23,6 +21,14 @@ function App() {
           <Spinner />
         ) : (
           <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              }
+            />
             <Route
               path="/apply-doctor"
               element={
@@ -85,30 +91,6 @@ function App() {
                 <PublicRoute>
                   <Register />
                 </PublicRoute>
-              }
-            />
-            <Route
-              path="/appointments"
-              element={
-                <ProtectedRoute>
-                  <Appointments />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/doctor-appointments"
-              element={
-                <ProtectedRoute>
-                  <DoctorAppointments />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <HomePage />
-                </ProtectedRoute>
               }
             />
           </Routes>
