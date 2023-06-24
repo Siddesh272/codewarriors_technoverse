72d1587e5eb5eab9538a594ac14afe5f30fc7367
KrishilShah <shahkrishil99@gmail.com>
2023-06-24 23:02:20 +0530

    dr availability and appointment list

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { useSelector } from "react-redux";
-import Spinner from "./components/Spinner";
+import Spinner from "./components/Spinner"; 
 import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
 import ApplyDoctor from "./pages/ApplyDoctor";
@@ -12,6 +12,8 @@ import Users from "./pages/admin/Users";
 import Doctors from "./pages/admin/Doctors";
 import Profile from "./pages/doctor/Profile";
 import BookingPage from "./pages/BookingPage";
+import Appointments from "./pages/Appointments";
+import DoctorAppointments from "./pages/doctor/DoctorAppointments";
 function App() {
   const { loading } = useSelector((state) => state.alerts);
   return (
@@ -21,14 +23,6 @@ function App() {
           <Spinner />
         ) : (
           <Routes>
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <HomePage />
-                </ProtectedRoute>
-              }
-            />
             <Route
               path="/apply-doctor"
               element={
@@ -91,6 +85,30 @@ function App() {
                 <PublicRoute>
                   <Register />
                 </PublicRoute>
+              }
+            />
+            <Route
+              path="/appointments"
+              element={
+                <ProtectedRoute>
+                  <Appointments />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/doctor-appointments"
+              element={
+                <ProtectedRoute>
+                  <DoctorAppointments />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
               }
             />
           </Routes>
--- client/src/Data/data.js
+++ client/src/Data/data.js
@@ -46,3 +46,25 @@ export const adminMenu = [
     icon: "fa-solid fa-user",
   },
 ];
+export const userMenu = [
+  {
+    name: "Home",
+    path: "/",
+    icon: "fa-solid fa-house",
+  },
+  {
+    name: "Appointments",
+    path: "/appointments",
+    icon: "fa-solid fa-list",
+  },
+  {
+    name: "Apply Doctor",
+    path: "/apply-doctor",
+    icon: "fa-solid fa-user-doctor",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+    icon: "fa-solid fa-user",
+  },
+];
