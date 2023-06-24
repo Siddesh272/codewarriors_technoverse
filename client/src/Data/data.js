043922d35a885236302be99ab5415a4ae944af26
Darshan Jain <darshanjain768@gmail.com>
2023-06-24 18:01:30 +0530

    user and admin

--- client/src/Data/data.js
+++ client/src/Data/data.js
@@ -25,3 +25,28 @@ export const SidebarMenu = [
     icon: "fa-solid fa-right-from-bracket",
   },
 ];
+
+// admin menu
+export const adminMenu = [
+  {
+    name: "Home",
+    path: "/",
+    icon: "fa-solid fa-house",
+  },
+
+  {
+    name: "Doctors",
+    path: "/admin/doctors",
+    icon: "fa-solid fa-user-doctor",
+  },
+  {
+    name: "Users",
+    path: "/admin/users",
+    icon: "fa-solid fa-user",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+    icon: "fa-solid fa-user",
+  },
+];
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,41 +1,66 @@
 import React from "react";
 import "../styles/LayoutStyles.css";
-import { SidebarMenu } from "./../Data/data";
+import { adminMenu, userMenu } from "./../Data/data";
 
 import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Layout = ({ children }) => {
+  const { user } = useSelector((state) => state.user);
   const location = useLocation();
-  return (
-    <>
-      <div className="main">
-        <div className="layout">
-          <div className="sidebar">
-            <div className="logo">
-              <h6>DOC APP</h6>
-              <hr />
-            </div>
-            <div className="menu">
-              {SidebarMenu.map((menu) => {
-                const isActive = location.pathname === menu.path;
-                return (
-                  <>
-                    <div className={`menu-item ${isActive && "active"}`}>
-                      <i className={menu.icon}></i>
-                      <Link to={menu.path}>{menu.name}</Link>
-                    </div>
-                  </>
-                );
-              })}
-            </div>
-          </div>
-          <div className="content">
-            <div className="header">Header</div>
-            <div className="body">{children}</div>
+// redering menu list
+const SidebarMenu = user?.isAdmin
+? adminMenu
+: user?.isDoctor
+? doctorMenu
+: userMenu;
+return (
+<>
+  <div className="main">
+    <div className="layout">
+      <div className="sidebar">
+        <div className="logo">
+          <h6 className="text-light">DOC APP</h6>
+          <hr />
+        </div>
+        <div className="menu">
+          {SidebarMenu.map((menu) => {
+            const isActive = location.pathname === menu.path;
+            return (
+              <>
+                <div className={`menu-item ${isActive && "active"}`}>
+                  <i className={menu.icon}></i>
+                  <Link to={menu.path}>{menu.name}</Link>
+                </div>
+              </>
+            );
+          })}
+          <div className={`menu-item `} onClick={handleLogout}>
+            <i className="fa-solid fa-right-from-bracket"></i>
+            <Link to="/login">Logout</Link>
           </div>
         </div>
       </div>
-    </>
-  );
+      <div className="content">
+        <div className="header">
+          <div className="header-content" style={{ cursor: "pointer" }}>
+            <Badge
+              count={user && user.notifcation.length}
+              onClick={() => {
+                navigate("/notification");
+              }}
+            >
+              <i class="fa-solid fa-bell"></i>
+            </Badge>
+
+            <Link to="/profile">{user?.name}</Link>
+          </div>
+        </div>
+        <div className="body">{children}</div>
+      </div>
+    </div>
+  </div>
+</>
+);
 };
 
 export default Layout;
