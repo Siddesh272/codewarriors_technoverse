16f718c249fb69edf69e7a4caab85173a60e9ca0
Darshan Jain <darshanjain768@gmail.com>
2023-06-25 03:36:56 +0530

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,6 @@ import BookingPage from "./pages/BookingPage";
 import Appointments from "./pages/Appointments";
 import DoctorAppointments from "./pages/doctor/DoctorAppointments";
 import Rating from "./pages/Rating";
-import "../styles/LayoutStyles.css";
 function App() {
   const { loading } = useSelector((state) => state.alerts);
   return (
