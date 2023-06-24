a6de637a15abe9a2458583c722b1db186f2bb1eb
Darshan Jain <darshanjain768@gmail.com>
2023-06-24 18:45:24 +0530

    apply doctor

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -19,3 +19,19 @@ router.post("/register", registerController);
 //Auth || POST
 router.post("/getUserData", authMiddleware, authController);
 module.exports = router;
+
+//Apply Doctor || POST
+router.post("/apply-doctor", authMiddleware, applyDoctorController);
+
+//Notifiaction  Doctor || POST
+router.post(
+  "/get-all-notification",
+  authMiddleware,
+  getAllNotificationController
+);
+//Notifiaction  Doctor || POST
+router.post(
+  "/delete-all-notification",
+  authMiddleware,
+  deleteAllNotificationController
+);
